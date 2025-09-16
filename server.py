b435c4a5930981f896a074640ee2c330734ed27d
javier <alexandersoriano1022@gmail.com>
2025-09-16 07:59:07 -0600

    enviar datos desde el cliente al servidos

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from urllib import parse
 port = 3000
 class miServidor(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
             self.path = "/index.html"
             return SimpleHTTPRequestHandler.do_GET(self)
+        
+        
+    def do_POST(self):
+            longitud = int(self.headers['Content-Length'])
+            datos = self.rfile.read(longitud)
+            datos = datos.decode("utf-8")
+            datos = parse.unquote(datos)
+            print(datos)
+
+
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(datos.encode("utf-8"))
 
 print("servidor ejecutandose")
 server= HTTPServer(("localhost",port),miServidor)
