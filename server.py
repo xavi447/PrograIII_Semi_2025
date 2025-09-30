2791a8958714bcc724c0bed3146574ec409c4301
javier <alexandersoriano1022@gmail.com>
2025-09-30 07:37:59 -0600

    uso de SPA para el sistema Academico

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from urllib import parse
+from urllib.parse import urlparse, parse_qs
 import json 
 import crud_alumno
 
@@ -9,6 +10,10 @@ crudAlumno = crud_alumno.crud_alumno()
 
 class miServidor(SimpleHTTPRequestHandler):
     def do_GET(self):
+        url_parseada = urlparse(self.path)
+        path = url_parseada.path
+        parametros = parse_qs(url_parseada.query)
+
         if self.path=="/":
             self.path="index.html"
             return SimpleHTTPRequestHandler.do_GET(self)
@@ -17,6 +22,9 @@ class miServidor(SimpleHTTPRequestHandler):
             self.send_response(200)
             self.end_headers()
             self.wfile.write(json.dumps(alumnos).encode('utf-8'))
+        if path=="/vistas":
+            self.path = '/modulos/'+ parametros['form'][0] +'.html'
+            return SimpleHTTPRequestHandler.do_GET(self)
     
     def do_POST(self):
         longitud = int(self.headers['Content-Length'])
